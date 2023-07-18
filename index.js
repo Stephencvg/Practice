225f34d99a6a77c9d9c707241292d7fd0246bb49
Stephencvg <stephenadeoye3506@mail.com>
2023-07-18 15:58:07 +0100

    add paragraph and add a change when the button is clicked

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const btn = document.querySelector("button");
 const head = document.querySelector("h1");
+const para = document.querySelector("p");
 
 btn.addEventListener("click", ()=> {
   head.style.color = "blue"
+  para.style.color = "white"
+  para.style.background = "grey";
 })
